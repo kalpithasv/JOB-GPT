e5a3ef83baa2f5570e59e589173c6ae90b7ae0e7
kalpithasv <kalpithasv@gmail.com>
2023-06-06 12:17:58 +0530

    :)

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
-import React from 'react';
-import Auth from './Pages/Auth';
+import './App.css';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+import SignupPage from './Pages/Signup';
+import LoginPage from './Pages/Login';
 
-const App = () => {
+function App() {
   return (
-    <div>
-      <Auth />
+    <div className="min-h-full h-screen flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+    <div className="max-w-md w-full space-y-8">
+     <BrowserRouter>
+        <Routes>
+            <Route path="/" element={<LoginPage/>} />
+            <Route path="/signup" element={<SignupPage/>} />
+        </Routes>
+      </BrowserRouter>
     </div>
+  </div>
   );
-};
+}
 
 export default App;
---
+++ src/Pages/Login.js
@@ -0,0 +1,16 @@
+import Header from "../Components/Header"
+import Login from "../Components/Login"
+
+export default function LoginPage(){
+    return(
+        <>
+             <Header
+                heading="Login to your account"
+                paragraph="Don't have an account yet? "
+                linkName="Signup"
+                linkUrl="/signup"
+                />
+            <Login/>
+        </>
+    )
+}
